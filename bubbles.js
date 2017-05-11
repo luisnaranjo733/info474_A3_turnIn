94155ab163f9c32ee464b506be956048cf9507e7
studentuser <studentuser@studentusers-MacBook-Pro.local>
2017-05-11 00:48:28 -0700

    doneish

---
+++ bubbles.js
@@ -0,0 +1,155 @@
+
+//
+function bubbles() {
+
+//local variables/defaults for example
+    var width = 960,
+        height = 960,
+        columnForColors = "cardType",
+        columnForRadius = "orderAmount",
+        identifer = "id";
+        strength = -50,
+        range = [5,30]
+        title = "Insert title";
+
+//function to create graph
+    function chart(selection) {
+        var data = selection.enter().data();
+        var div = selection,
+            svg = div.selectAll('svg'),
+            head = div.selectAll('head');
+            var head = selection
+            .append('div')
+            .style("color", "black")
+            .text(title)
+            .style("left", "100px")
+            .style("top", "100px")
+            .style("position", "absolute")
+            .style("font-size", "100px");
+        svg.attr('width', width).attr('height', height);
+
+        //set up hover over
+        var tooltip = selection
+            .append("div")
+            .style("position", "absolute")
+            .style("visibility", "hidden")
+            .style("color", "white")
+            .style("padding", "8px")
+            .style("background-color", "#626D71")
+            .style("border-radius", "6px")
+            .style("text-align", "center")
+            .style("font-family", "monospace")
+            .style("width", "400px")
+
+        //create force push on bubbles
+        var simulation = d3.forceSimulation(data)
+            .force("charge", d3.forceManyBody().strength(strength))
+            .force("x", d3.forceX())
+            .force("y", d3.forceY())
+            .on("tick", ticked);
+
+        function ticked(e) {
+            node.attr("cx", function(d) {
+                    return d.x;
+                })
+                .attr("cy", function(d) {
+                    return d.y;
+                });
+        }
+
+        var colorCircles = d3.scaleOrdinal(d3.schemeCategory10);
+        var scaleRadius = d3.scaleLinear().domain([d3.min(data, function(d) {
+            return +d[columnForRadius];
+        }), d3.max(data, function(d) {
+            return +d[columnForRadius];
+        })]).range(range)
+
+//build circles
+        var node = svg.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr('r', function(d) {
+                return scaleRadius(d[columnForRadius])
+            })
+            .style("fill", function(d) {
+                return colorCircles(d[columnForColors])
+            })
+            .attr('transform', 'translate(' + [width / 2, height / 2] + ')')
+            .on("mouseover", function(d) {
+                tooltip.html(columnForColors+ ": " + d[columnForColors] + "<br>" + identifer + ": " + d[identifer] + "<br>" + columnForRadius +": " + d[columnForRadius] );
+                return tooltip.style("visibility", "visible");
+            })
+            .on("mousemove", function() {
+                return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
+            })
+            .on("mouseout", function() {
+                return tooltip.style("visibility", "hidden");
+            });
+    }
+
+
+//getter/setter methods
+    chart.strength = function(value) {
+        if (!arguments.length) {
+            return strength;
+        }
+        strength = value;
+        return chart;
+    };
+
+    chart.identifer = function(value) {
+        if (!arguments.length) {
+            return identifer;
+        }
+        identifer = value;
+        return chart;
+    };
+
+
+    chart.title = function(value) {
+        if (!arguments.length) return title;
+        title = value;
+        return chart;
+    };
+
+    chart.columnForRadius = function(value) {
+        if (!arguments.length) return columnForRadius;
+        columnForRadius = value;
+        return chart;
+    };
+
+    chart.columnForColors = function(value) {
+        if (!arguments.length) return columnForColors;
+        columnForColors = value;
+        return chart;
+    };
+
+
+
+    chart.range = function(value) {
+        if (!arguments.length) {
+            return range;
+        }
+        range = value;
+        return chart;
+    };
+
+    chart.width = function(value) {
+        if (!arguments.length) {
+            return width;
+        }
+        width = value;
+        return chart;
+    };
+
+    chart.height = function(value) {
+        if (!arguments.length) {
+            return height;
+        }
+        height = value;
+        return chart;
+    };
+
+    return chart;
+}
---
+++ readme.md
@@ -0,0 +1,54 @@
+# Iman Baghai
+
+# Bubbles plot
+
+![Complete example](./imgs/overView.png)
+
+This is an example of the bubble graph created by the api.
+
+This type of graph in the example has purchases with dot sizes relative to the size of the purchase amount and the dots colors are determined by the credit card type used to purchase. By hovering over a dot you can get more details like card type, the order id, and the purchase amount. This graph also allows for the changing of how close or far you want the dots to be with one another.
+
+This is a good graph if you have categorical information you want to break out into groups and gain characteristics for each single input as well.
+
+In this example the data in this example is structured as is:
+
+
+| id        | cardType           | PurchaseAmount  |
+| ------------- |:-------------:| :-----:|
+| 1   | visa | 20 |
+| 2    | mastercard  | 12 |
+| 3 | visa      |    10 |
+
+In this data set you can set the how you want the colors of the dots to be grouped by, what data should be used to create dot sizes, and a data id. Currently, the chart is designed to support three main columns of data that this chart can support. Or more technically speaking it is preferable to have your data in a long format rather than a wide format.
+
+To create this chart do the following:
+
+- Load your data
+- create  ```chart variable``` calling ```bubbles``` and passing in parameters and specifications as needed and desired
+- call your ```chart variable``` into a div that'd you like passing in your data and ```call``` your ```chart``` variable
+
+visual skeleton example:
+
+```var myChart = chart().param1(value1).param2(value2);```
+
+```var chartWrapper = d3.select('#my-div').datum([dataSet]).call(myChart);```
+
+
+Here is a table of methods for the API that can be called on bubbles that can be passed into myChart to set it up accordingly 
+
+## Bubbles methods
+
+
+| Method        | Input + example        | Output  
+| ------------- |:-------------:| :-----:|
+| chart.width   | integer, chart.width(500) | set width of chart |
+| chart.height  | integer, chart.height(500)  | set height of chart|
+| chart.range | array of 2 integers, chart.range([10,30])      |    set range of min and max size of bubbles|
+| chart.columnForColors| String, chart.columnForColors('cardType') | Sets column of data for grouping of bubble colors and hover on features |
+| chart.columnForRadius | string,  chart.columnForRadius('purchaseAmount')| sets column for setting bubble size and features for hover on display  |
+|chart.title | string, chart.title('title') | Sets title for graph |
+|chart.identifer| string, chart.identifer('id') | sets up another descriptor for hover over from column of data |
+|chart.strength | integer (recommend >0 #s ), chart.strength(-100) | sets the power in which the bubbles repel off on another to add more or less spacing between them |
+
+
+link to an example
